97cc9210e3e5f6cb8b2f6eb43cb2e8be687f198a
Copeik <33516078+Copeik@users.noreply.github.com>
2019-02-17 00:33:30 +0100

    fix

--- src/main/java/com/copeik/hibernate/controller/PedidosController.java
+++ src/main/java/com/copeik/hibernate/controller/PedidosController.java
@@ -1,5 +1,8 @@
 package com.copeik.hibernate.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -17,8 +20,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.copeik.hibernate.converter.PedidosConvertidor;
+import com.copeik.hibernate.entity.Clientes;
 import com.copeik.hibernate.entity.Especificaciones;
 import com.copeik.hibernate.entity.Pedidos;
+import com.copeik.hibernate.entity.Trabajadores;
 import com.copeik.hibernate.model.MArticulos;
 import com.copeik.hibernate.model.MPedidos;
 import com.copeik.hibernate.repository.PedidosRepositorio;
@@ -38,9 +43,24 @@ public class PedidosController {
 	}
 	
 	@PostMapping("/pedidos")
-	public boolean Actualizar(@RequestBody @Valid Pedidos pedidos) {
-		return service.actualizar(pedidos);
+		    public boolean Aniadir2 (@RequestParam(value="cliente", required=true) int cliente, 
+		            @RequestParam(value="fecha", required=true) String fecha,@RequestParam(value="entregado", required=true) boolean entregado, 
+		            @RequestParam(value="descripcion", required=true) String descripcion,@RequestParam(value="total", required=true) double total,@RequestParam(value="codtrabajador", required=true) int codtrabajador) throws ParseException {
+		Clientes a = new Clientes();
+		a.setCod_cliente(cliente);
+		Trabajadores tra = new Trabajadores();
+		tra.setCod_trabajador(codtrabajador);
+		SimpleDateFormat formatoDelTexto = new SimpleDateFormat("yyyy-MM-dd");
+		Date fecha_cad2 = null;
+		fecha_cad2 = formatoDelTexto.parse(fecha);
+		    	Pedidos pedidos = new Pedidos(a,fecha_cad2,entregado,descripcion,total,tra);
+		    	return service.actualizar(pedidos);
+	
 	}
+	//@PostMapping("/pedidos")
+	//public boolean Actualizar(@RequestBody @Valid Pedidos pedidos) {
+	//	return service.actualizar(pedidos);
+	//}
 	
 	@DeleteMapping("/pedidos")
 	public boolean borrar(@RequestParam(value="cod_pedido", required=true) int cod_pedido) {
